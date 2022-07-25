8917bf9f8dbe357a9fe1fe7e62ca777c83f24b24
kevin De Jesus <sebadjkevin@gmail.com>
2022-07-25 04:34:16 -0300

    carrousel

--- src/Components/OverlayPoster/index.jsx
+++ src/Components/OverlayPoster/index.jsx
@@ -1,11 +1,13 @@
-
+import './../../styles/overlayposter.scss'
 
 
 const OvPosterCard=({title,poster})=>{
     return (<div className="overlay-poster">
-        <img className="overlay-poster-image" width={60} src={`https://image.tmdb.org/t/p/original/${poster}`} alt={title}/>
+        <img className="overlay-poster-image" src={`https://image.tmdb.org/t/p/original/${poster}`} alt={title}/>
         <div className="overlay-circle">
             <p>{title}</p>
         </div>
     </div>)
 }
+
+export default OvPosterCard
--- src/Components/Poster/index.jsx
+++ src/Components/Poster/index.jsx
@@ -1,16 +1,36 @@
+import {useState} from 'react'
 import { useGeTrendingWeekQuery } from "../../services/api"
+import {Swiper,SwiperSlide} from 'swiper/react'
+import {Autoplay,Navigation,Pagination} from 'swiper';
 import './../../styles/posterhom.scss'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import OvPosterCard from "../OverlayPoster";
+
 
 const Poster=()=>{
+    const [item,setItem]= useState(null)
     const {currentData,error,isFetching} = useGeTrendingWeekQuery()
     if(isFetching)return (<div>Loading ...</div>)
     if(error)return(<div>Ops Error 400</div>)
     if(currentData.length){
         return (<>
-        {<div className="poster-content">
-        <img className='image-poster' src={`https://image.tmdb.org/t/p/original/${currentData[Math.round(Math.random()* currentData.length)].backdrop_path}`}/>
-        
-        </div>}
+        {<Swiper
+         onActiveIndexChange={(e)=> setItem(currentData[e.activeIndex])}
+         autoplay={{
+            delay:1000*10,
+            disableOnInteraction:false
+        }}
+         centeredSlides={true}
+         modules={[Autoplay,Navigation,Pagination]}
+         className="mySwiperbig"
+        >
+            {item && <OvPosterCard title={item.title} poster={item.poster_path}/>}
+            {currentData.map((e,i)=><SwiperSlide className={`swiper-slide ${i}`}  key={e.id}>
+            <img className='image-poster' src={`https://image.tmdb.org/t/p/original/${e.backdrop_path}`}/>
+                </SwiperSlide>)}
+        </Swiper>}
         </>)
     }
    
